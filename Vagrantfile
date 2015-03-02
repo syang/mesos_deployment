bd01397fe55bb2dd5620007f9faf854630466d74
syang <shuo.yang.2006@gmail.com>
2015-03-02 15:14:30 -0800

    setup the vagrant testing bed

---
+++ Vagrantfile
@@ -0,0 +1,25 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+require 'yaml'
+
+hosts_file = YAML.load_file('hosts_vagrant.yml')
+hosts = hosts_file['hosts']
+
+os_family = "precise64"
+box_url = "http://files.vagrantup.com/precise64.box"
+vm_memory_mb = 512
+
+Vagrant.configure("2") do |config|
+  hosts.each do |name, ip|
+    config.vm.define name do |machine|
+      machine.vm.box = os_family
+      machine.vm.box_url = box_url
+      machine.vm.hostname = "%s.example.org" % name
+      machine.vm.network :private_network, ip: ip
+      machine.vm.provider "virtualbox" do |v|
+          v.name = name
+          v.customize ["modifyvm", :id, "--memory", vm_memory_mb]
+      end
+    end
+  end
+end
---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+machine_type=$(echo $1 | tr '[:lower:]' '[:upper:]')
+os_family=$(echo $2 | tr '[:lower:]' '[:upper:]')
+
+printf "\n$machine_type\t$os_family\n\n"
+
+#rm -rf ./Vagrantfile
+#source rc
+
+if [ x$machine_type = xVAGRANT ]; then
+  if [ ! x$os_family = xDEBIAN ] && [ ! x$os_family = xREDHAT ]; then
+    echo "Usage: run.sh [vagrant [debian | redhat] | physical <inventory>] "
+    exit 2
+  fi
+  vagrant up
+  export ANSIBLE_CONFIG=.ansible.cfg.vagrant
+#  ansible-playbook -i inventories/hosts_vagrant_ansible site.yml
+elif [ x$machine_type = xPHYSICAL ]; then
+  export ANSIBLE_CONFIG=.ansible.cfg.physical
+#  ansible-playbook -i inventories/$2 site.yml
+else
+  echo "Usage: run.sh [vagrant [debian | redhat] | physical <inventory>] "
+  exit 1
+fi
